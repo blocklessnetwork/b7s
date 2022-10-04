6b9ae8898684c6ef2b96a321e6f1b99e01f73318
Derek Anderson <derek@akash.network>
2022-10-04 09:56:26 -0500

    omit empty

--- src/models/repository.go
+++ src/models/repository.go
@@ -1,37 +1,37 @@
 package models
 
 type FunctionManifest struct {
-	Function   Function   `json:"function"`
-	Deployment Deployment `json:"deployment"`
+	Function   Function   `json:"function,omitempty"`
+	Deployment Deployment `json:"deployment,omitempty"`
 }
 type Function struct {
-	ID           string   `json:"id"`
-	Name         string   `json:"name"`
-	Version      string   `json:"version"`
-	BuildCommand string   `json:"build-command"`
-	BuildOutput  string   `json:"build-output"`
-	Runtime      string   `json:"runtime"`
-	Extensions   []string `json:"extensions"`
+	ID           string   `json:"id,omitempty"`
+	Name         string   `json:"name,omitempty"`
+	Version      string   `json:"version,omitempty"`
+	BuildCommand string   `json:"build-command,omitempty"`
+	BuildOutput  string   `json:"build-output,omitempty"`
+	Runtime      string   `json:"runtime,omitempty"`
+	Extensions   []string `json:"extensions,omitempty"`
 }
 type Arguments struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
 }
 type Envvars struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
 }
 type Methods struct {
-	Name      string      `json:"name"`
-	Entry     string      `json:"entry"`
-	Arguments []Arguments `json:"arguments"`
-	Envvars   []Envvars   `json:"envvars"`
+	Name      string      `json:"name,omitempty"`
+	Entry     string      `json:"entry,omitempty"`
+	Arguments []Arguments `json:"arguments,omitempty"`
+	Envvars   []Envvars   `json:"envvars,omitempty"`
 }
 type Deployment struct {
-	Checksum    string    `json:"checksum"`
-	URI         string    `json:"uri"`
-	Permission  string    `json:"permission"`
-	Methods     []Methods `json:"methods"`
-	Aggregation string    `json:"aggregation"`
-	Nodes       int       `json:"nodes"`
+	Checksum    string    `json:"checksum,omitempty"`
+	URI         string    `json:"uri,omitempty"`
+	Permission  string    `json:"permission,omitempty"`
+	Methods     []Methods `json:"methods,omitempty"`
+	Aggregation string    `json:"aggregation,omitempty"`
+	Nodes       int       `json:"nodes,omitempty"`
 }
