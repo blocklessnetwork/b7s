ac8f827a7763a7e7f27e1d91cfe31539d2618836
Derek Anderson <derek@akash.network>
2022-10-03 23:10:14 -0500

    move download client

--- src/http/client.go
+++ src/http/client.go
@@ -1,9 +1,14 @@
 package http
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"time"
+
+	"github.com/blocklessnetworking/b7s/src/models"
+	"github.com/cavaliergopher/grab/v3"
+	log "github.com/sirupsen/logrus"
 )
 
 var RestClient = &http.Client{Timeout: 10 * time.Second}
@@ -16,4 +21,39 @@ func GetJson(url string, target interface{}) error {
 	defer r.Body.Close()
 
 	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func Download(ctx context.Context, functionManifest models.FunctionManifest) error {
+	WorkSpaceRoot := ctx.Value("config").(models.Config).Node.WorkSpaceRoot
+	client := grab.NewClient()
+	req, _ := grab.NewRequest(WorkSpaceRoot+"/"+functionManifest.Id, functionManifest.Runtime.Uri)
+	resp := client.Do(req)
+
+	log.WithFields(log.Fields{
+		"uri": functionManifest.Runtime.Uri,
+	}).Info("function scheduled for sync")
+
+	// start UI loop
+	t := time.NewTicker(500 * time.Millisecond)
+	defer t.Stop()
+Loop:
+	for {
+		select {
+		case <-t.C:
+			log.WithFields(log.Fields{
+				"uri": functionManifest.Runtime.Uri,
+			}).Info("function sync progress")
+
+		case <-resp.Done:
+			// download is complete
+			break Loop
+		}
+	}
+
+	// check for errors
+	if err := resp.Err(); err != nil {
+		log.WithFields(log.Fields{
+			"uri": functionManifest.Runtime.Uri,
+		}).Info("function sync field will try again")
+	}
 }
--- src/repository/json.go
+++ src/repository/json.go
@@ -2,11 +2,9 @@ package repository
 
 import (
 	"context"
-	"time"
 
 	"github.com/blocklessnetworking/b7s/src/http"
 	"github.com/blocklessnetworking/b7s/src/models"
-	"github.com/cavaliergopher/grab/v3"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -15,7 +13,6 @@ type JSONRepository struct{}
 // get the manifest from the repository
 // downloads the binary
 func (r JSONRepository) Get(ctx context.Context, manifestPath string) models.FunctionManifest {
-	WorkSpaceRoot := ctx.Value("config").(models.Config).Node.WorkSpaceRoot
 
 	functionManifest := models.FunctionManifest{}
 	err := http.GetJson(manifestPath, &functionManifest)
@@ -24,37 +21,7 @@ func (r JSONRepository) Get(ctx context.Context, manifestPath string) models.Fun
 		log.Warn(err)
 	}
 
-	client := grab.NewClient()
-	req, _ := grab.NewRequest(WorkSpaceRoot+"/"+functionManifest.Id, functionManifest.Runtime.Uri)
-	resp := client.Do(req)
-
-	log.WithFields(log.Fields{
-		"uri": functionManifest.Runtime.Uri,
-	}).Info("function scheduled for sync")
-
-	// start UI loop
-	t := time.NewTicker(500 * time.Millisecond)
-	defer t.Stop()
-Loop:
-	for {
-		select {
-		case <-t.C:
-			log.WithFields(log.Fields{
-				"uri": functionManifest.Runtime.Uri,
-			}).Info("function sync progress")
-
-		case <-resp.Done:
-			// download is complete
-			break Loop
-		}
-	}
-
-	// check for errors
-	if err := resp.Err(); err != nil {
-		log.WithFields(log.Fields{
-			"uri": functionManifest.Runtime.Uri,
-		}).Info("function sync field will try again")
-	}
+	http.Download(ctx, functionManifest)
 
 	log.WithFields(log.Fields{
 		"uri": functionManifest.Runtime.Uri,
