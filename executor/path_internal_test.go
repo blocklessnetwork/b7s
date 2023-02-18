73c5efae44d98b085a2cfe0d5c4f47953d12b37c
Maelkum <pathlidge.ann+github@gmail.com>
2023-02-18 06:22:46 +0100

    Add test for paths created by the executor (manifest, entry etc)

---
+++ executor/path_internal_test.go
@@ -0,0 +1,37 @@
+package executor
+
+import (
+	"testing"
+
+	"github.com/blocklessnetworking/b7s/testing/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExecutor_RequestPaths(t *testing.T) {
+
+	const (
+		workdir        = "/var/tmp/b7s/workspace"
+		requestID      = "request-id"
+		functionID     = "function-id"
+		functionMethod = "function-method"
+
+		// Expected paths.
+		expectedRequestWorkdir = workdir + "/t/request-id"
+		expectedFSRoot         = workdir + "/t/request-id/fs"
+		expectedManifestPath   = workdir + "/t/request-id/runtime-manifest.json"
+		expectedEntry          = workdir + "/function-id/function-method"
+	)
+
+	executor := &Executor{
+		log: mocks.NoopLogger,
+		cfg: Config{
+			WorkDir: workdir,
+		},
+	}
+
+	paths := executor.generateRequestPaths(requestID, functionID, functionMethod)
+	assert.Equal(t, expectedRequestWorkdir, paths.workdir)
+	assert.Equal(t, expectedEntry, paths.entry)
+	assert.Equal(t, expectedFSRoot, paths.fsRoot)
+	assert.Equal(t, expectedManifestPath, paths.manifest)
+}
