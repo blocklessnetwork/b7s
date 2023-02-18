8981076f05aadeaf6026b964f792d120ef0b150a
Maelkum <pathlidge.ann+github@gmail.com>
2023-02-18 03:23:58 +0100

    Add tests with a baseline cache to cover all storage failure scenarios

--- peerstore/peerstore_test.go
+++ peerstore/peerstore_test.go
@@ -3,15 +3,16 @@ package peerstore_test
 import (
 	"testing"
 
+	"github.com/cockroachdb/pebble"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multiaddr"
+	"github.com/stretchr/testify/require"
+
 	"github.com/blocklessnetworking/b7s/models/blockless"
 	"github.com/blocklessnetworking/b7s/peerstore"
 	"github.com/blocklessnetworking/b7s/store"
 	"github.com/blocklessnetworking/b7s/testing/helpers"
 	"github.com/blocklessnetworking/b7s/testing/mocks"
-	"github.com/cockroachdb/pebble"
-	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/multiformats/go-multiaddr"
-	"github.com/stretchr/testify/require"
 )
 
 func Test_PeerStore(t *testing.T) {
@@ -79,7 +80,7 @@ func Test_PeerStore(t *testing.T) {
 		require.Equal(t, addr.String(), peer.MultiAddr)
 		require.Equal(t, info, peer.AddrInfo)
 	})
-	t.Run("adding known peer", func(t *testing.T) {
+	t.Run("adding known peer to peer list", func(t *testing.T) {
 		t.Parallel()
 
 		peerstore, _, db := setupPeerStore(t)
@@ -107,6 +108,122 @@ func Test_PeerStore(t *testing.T) {
 	})
 }
 
+func Test_PeerStore_Store(t *testing.T) {
+
+	t.Run("handles failure to store peer", func(t *testing.T) {
+
+		store := mocks.BaselineStore(t)
+		store.GetRecordFunc = func(string, interface{}) error {
+			// We first check if the peer exists - make sure it doesn't.
+			return blockless.ErrNotFound
+		}
+		store.SetRecordFunc = func(string, interface{}) error {
+			return mocks.GenericError
+		}
+
+		peerstore := peerstore.New(store)
+
+		var (
+			peerID = mocks.GenericPeerID
+			addr   = genericMultiAddr(t)
+			info   = peer.AddrInfo{
+				ID:    peerID,
+				Addrs: []multiaddr.Multiaddr{addr},
+			}
+		)
+
+		err := peerstore.Store(peerID, addr, info)
+		require.ErrorIs(t, err, mocks.GenericError)
+	})
+	t.Run("handles failure to get existing peer", func(t *testing.T) {
+
+		store := mocks.BaselineStore(t)
+		store.GetRecordFunc = func(string, interface{}) error {
+			return mocks.GenericError
+		}
+		peerstore := peerstore.New(store)
+
+		var (
+			peerID = mocks.GenericPeerID
+			addr   = genericMultiAddr(t)
+			info   = peer.AddrInfo{
+				ID:    peerID,
+				Addrs: []multiaddr.Multiaddr{addr},
+			}
+		)
+
+		err := peerstore.Store(peerID, addr, info)
+		require.ErrorIs(t, err, mocks.GenericError)
+	})
+	t.Run("handles noop on existing peer", func(t *testing.T) {
+
+		store := mocks.BaselineStore(t)
+		peerstore := peerstore.New(store)
+
+		var (
+			peerID = mocks.GenericPeerID
+			addr   = genericMultiAddr(t)
+			info   = peer.AddrInfo{
+				ID:    peerID,
+				Addrs: []multiaddr.Multiaddr{addr},
+			}
+		)
+
+		err := peerstore.Store(peerID, addr, info)
+		require.NoError(t, err)
+	})
+	t.Run("handles failure to get existing peer list", func(t *testing.T) {
+		store := mocks.BaselineStore(t)
+		store.GetRecordFunc = func(string, interface{}) error {
+			return mocks.GenericError
+		}
+
+		peerstore := peerstore.New(store)
+
+		var (
+			peerID = mocks.GenericPeerID
+			addr   = genericMultiAddr(t)
+			info   = peer.AddrInfo{
+				ID:    peerID,
+				Addrs: []multiaddr.Multiaddr{addr},
+			}
+		)
+
+		err := peerstore.UpdatePeerList(peerID, addr, info)
+		require.ErrorIs(t, err, mocks.GenericError)
+	})
+	t.Run("handles failure to update peer list", func(t *testing.T) {
+		store := mocks.BaselineStore(t)
+		store.SetRecordFunc = func(string, interface{}) error {
+			return mocks.GenericError
+		}
+
+		peerstore := peerstore.New(store)
+
+		var (
+			peerID = mocks.GenericPeerID
+			addr   = genericMultiAddr(t)
+			info   = peer.AddrInfo{
+				ID:    peerID,
+				Addrs: []multiaddr.Multiaddr{addr},
+			}
+		)
+
+		err := peerstore.UpdatePeerList(peerID, addr, info)
+		require.ErrorIs(t, err, mocks.GenericError)
+	})
+	t.Run("handles peer list retrieval error", func(t *testing.T) {
+		store := mocks.BaselineStore(t)
+		store.GetRecordFunc = func(string, interface{}) error {
+			return mocks.GenericError
+		}
+
+		peerstore := peerstore.New(store)
+
+		_, err := peerstore.Peers()
+		require.ErrorIs(t, err, mocks.GenericError)
+	})
+}
 func setupPeerStore(t *testing.T) (*peerstore.PeerStore, *store.Store, *pebble.DB) {
 	t.Helper()
 
