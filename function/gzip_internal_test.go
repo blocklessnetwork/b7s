699ace2678c03e71fd74c2c60e63435b232a8ec8
Maelkum <pathlidge.ann+github@gmail.com>
2023-02-27 15:22:10 +0100

    Add tests for archive unpacking code

---
+++ function/gzip_internal_test.go
@@ -0,0 +1,50 @@
+package function
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/blocklessnetworking/b7s/store"
+	"github.com/blocklessnetworking/b7s/testing/helpers"
+	"github.com/blocklessnetworking/b7s/testing/mocks"
+)
+
+func TestFunction_UnpackArchive(t *testing.T) {
+
+	const (
+		filename = "testdata/testFunction.tar.gz"
+	)
+
+	workdir, err := os.MkdirTemp("", "b7s-function-unpack-")
+	require.NoError(t, err)
+
+	defer os.RemoveAll(workdir)
+
+	store := store.New(helpers.InMemoryDB(t))
+	fh := NewHandler(mocks.NoopLogger, store, workdir)
+
+	err = fh.unpackArchive(filename, workdir)
+	require.NoError(t, err)
+}
+
+func TestFunction_UnpackArchiveHandlesErrors(t *testing.T) {
+	t.Run("handles missing archive", func(t *testing.T) {
+
+		const (
+			filename = "testdata/nonExistantFile.tar.gz"
+		)
+
+		workdir, err := os.MkdirTemp("", "b7s-function-unpack-")
+		require.NoError(t, err)
+
+		defer os.RemoveAll(workdir)
+
+		store := store.New(helpers.InMemoryDB(t))
+		fh := NewHandler(mocks.NoopLogger, store, workdir)
+
+		err = fh.unpackArchive(filename, workdir)
+		require.Error(t, err)
+	})
+}
