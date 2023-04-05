f8dd09aafd41cde3b3e8697120252abeddf6e3a8
Maelkum <pathlidge.ann+github@gmail.com>
2023-04-05 15:45:25 +0200

    Add function to list installed functions

--- fstore/fstore.go
+++ fstore/fstore.go
@@ -41,16 +41,7 @@ func New(log zerolog.Logger, store Store, workdir string) *FStore {
 	return &h
 }
 
-// Sync will go through all functions stored in the database and verify that the
-// files are still found in local storage.
-func (h *FStore) Sync() {
-
-	cids := h.store.Keys()
-
-	for _, cid := range cids {
-		err := h.syncFunction(cid)
-		if err != nil {
-			h.log.Error().Err(err).Str("cid", cid).Msg("could not sync function")
-		}
-	}
+// InstalledFunctions will return the CIDs of all functions found in local storage.
+func (h *FStore) InstalledFunctions() []string {
+	return h.store.Keys()
 }
