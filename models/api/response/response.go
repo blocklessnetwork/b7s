69f22ece8da8fa9f71c46377a508f3bd2c7aafa8
Maelkum <pathlidge.ann+github@gmail.com>
2023-04-07 15:26:02 +0200

    Keep API response for execution backwards compatible

--- models/api/response/response.go
+++ models/api/response/response.go
@@ -5,7 +5,14 @@ import (
 )
 
 // Execute describes the REST API response for function execution.
-type Execute execute.Result
+type Execute struct {
+	Code      string                `json:"code"`
+	Result    string                `json:"result"`
+	ResultEx  execute.RuntimeOutput `json:"result_ex"`
+	RequestID string                `json:"request_id"`
+	// NOTE: Not sending the usage information for now.
+	Usage execute.Usage `json:"-"`
+}
 
 // InstallFunction describes the REST API response for the function install.
 type InstallFunction struct {
