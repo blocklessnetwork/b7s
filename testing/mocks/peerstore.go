6b700aa46763675ea8b0405f4f98ec542055976c
Maelkum <pathlidge.ann+github@gmail.com>
2023-04-19 16:34:03 +0200

    Extend mocks peerstore

--- testing/mocks/peerstore.go
+++ testing/mocks/peerstore.go
@@ -10,24 +10,35 @@ import (
 )
 
 type PeerStore struct {
-	StoreFunc func(peer.ID, multiaddr.Multiaddr, peer.AddrInfo) error
-	PeersFunc func() ([]blockless.Peer, error)
+	GetFunc    func(peer.ID) (blockless.Peer, error)
+	StoreFunc  func(peer.ID, multiaddr.Multiaddr, peer.AddrInfo) error
+	PeersFunc  func() ([]blockless.Peer, error)
+	RemoveFunc func(peer.ID) error
 }
 
 func BaselinePeerStore(t *testing.T) *PeerStore {
 	t.Helper()
 
 	peerstore := PeerStore{
+		GetFunc: func(peer.ID) (blockless.Peer, error) {
+			return blockless.Peer{}, nil
+		},
 		StoreFunc: func(peer.ID, multiaddr.Multiaddr, peer.AddrInfo) error {
 			return nil
 		},
-
 		PeersFunc: func() ([]blockless.Peer, error) {
 			return []blockless.Peer{}, nil
+		},
+		RemoveFunc: func(peer.ID) error {
+			return GenericError
 		},
 	}
 
 	return &peerstore
+}
+
+func (p *PeerStore) Get(id peer.ID) (blockless.Peer, error) {
+	return p.GetFunc(id)
 }
 
 func (p *PeerStore) Store(id peer.ID, addr multiaddr.Multiaddr, info peer.AddrInfo) error {
@@ -36,4 +47,8 @@ func (p *PeerStore) Store(id peer.ID, addr multiaddr.Multiaddr, info peer.AddrIn
 
 func (p *PeerStore) Peers() ([]blockless.Peer, error) {
 	return p.PeersFunc()
+}
+
+func (p *PeerStore) Remove(id peer.ID) error {
+	return p.RemoveFunc(id)
 }
