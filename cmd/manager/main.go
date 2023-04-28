ea41a8425a5be5b421cb72e58b58a85328d8055e
Derek Anderson <dmikey@users.noreply.github.com>
2023-04-27 19:56:49 -0500

    use spf flag

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"context"
 	"crypto/rand"
-	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -14,6 +13,7 @@ import (
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/spf13/pflag"
 
 	golog "github.com/ipfs/go-log/v2"
 	ma "github.com/multiformats/go-multiaddr"
@@ -23,35 +23,26 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// LibP2P code uses golog to log messages. They log with different
-	// string IDs (i.e. "swarm"). We can control the verbosity level for
-	// all loggers with:
-	golog.SetAllLoggers(golog.LevelInfo) // Change to INFO for extra info
+	golog.SetAllLoggers(golog.LevelInfo)
 
-	// Parse options from the command line
-	listenF := flag.Int("l", 0, "wait for incoming connections")
-	insecureF := flag.Bool("insecure", false, "use an unencrypted connection")
-	seedF := flag.Int64("seed", 0, "set random seed for id generation")
-	flag.Parse()
+	listenF := pflag.IntP("listen", "l", 0, "wait for incoming connections")
+	insecureF := pflag.BoolP("insecure", "i", false, "use an unencrypted connection")
+	seedF := pflag.Int64P("seed", "s", 0, "set random seed for id generation")
+	pflag.Parse()
 
 	if *listenF == 0 {
 		log.Fatal("Please provide a port to bind on with -l")
 	}
 
-	// Make a host that listens on the given multiaddress
 	ha, err := makeBasicHost(*listenF, *insecureF, *seedF)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-		startListener(ctx, ha, *listenF, *insecureF)
-		// Run until canceled.
-		<-ctx.Done()
-	
+	startListener(ctx, ha, *listenF, *insecureF)
+	<-ctx.Done()
 }
 
-// makeBasicHost creates a LibP2P host with a random peer ID listening on the
-// given multiaddress. It won't encrypt the connection if insecure is true.
 func makeBasicHost(listenPort int, insecure bool, randseed int64) (host.Host, error) {
 	var r io.Reader
 	if randseed == 0 {
@@ -60,8 +51,6 @@ func makeBasicHost(listenPort int, insecure bool, randseed int64) (host.Host, er
 		r = mrand.New(mrand.NewSource(randseed))
 	}
 
-	// Generate a key pair for this host. We will use it at least
-	// to obtain a valid host ID.
 	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
 	if err != nil {
 		return nil, err
@@ -81,11 +70,7 @@ func makeBasicHost(listenPort int, insecure bool, randseed int64) (host.Host, er
 }
 
 func getHostAddress(ha host.Host) string {
-	// Build host multiaddress
 	hostAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/p2p/%s", ha.ID().Pretty()))
-
-	// Now we can build a full multiaddress to reach this host
-	// by encapsulating both addresses:
 	addr := ha.Addrs()[0]
 	return addr.Encapsulate(hostAddr).String()
 }
@@ -94,8 +79,6 @@ func startListener(ctx context.Context, ha host.Host, listenPort int, insecure b
 	fullAddr := getHostAddress(ha)
 	log.Printf("I am %s\n", fullAddr)
 
-	// Set a stream handler on host A. /echo/1.0.0 is
-	// a user-defined protocol name.
 	ha.SetStreamHandler("/echo/1.0.0", func(s network.Stream) {
 		log.Println("listener received new stream")
 		if err := doEcho(s); err != nil {
@@ -109,13 +92,12 @@ func startListener(ctx context.Context, ha host.Host, listenPort int, insecure b
 	log.Println("listening for connections")
 
 	if insecure {
-		log.Printf("Now run \"./echo -l %d -d %s -insecure\" on a different terminal\n", listenPort+1, fullAddr)
+		log.Printf("Now run \"./echo -l %d -d %s --insecure\" on a different terminal\n", listenPort+1, fullAddr)
 	} else {
 		log.Printf("Now run \"./echo -l %d -d %s\" on a different terminal\n", listenPort+1, fullAddr)
 	}
 }
 
-// doEcho reads a line of data a stream and writes it back
 func doEcho(s network.Stream) error {
 	buf := bufio.NewReader(s)
 	str, err := buf.ReadString('\n')
