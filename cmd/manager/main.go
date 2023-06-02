9c5b36cf9e8654ce0cf38b5af62d86b7d9a4d75a
Derek Anderson <dmikey@users.noreply.github.com>
2023-06-02 10:21:33 -0500

    change channel

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -102,7 +102,7 @@ func startListener(ctx context.Context, ha host.Host, listenPort int, insecure b
 	fullAddr := getHostAddress(ha)
 	logger.Info().Msgf("I am %s", fullAddr)
 
-	ha.SetStreamHandler("/echo/1.0.0", func(s network.Stream) {
+	ha.SetStreamHandler("/b7s/manager/1.0.0", func(s network.Stream) {
 		if allowedPeer == "" && s.Conn().RemotePeer().Pretty() != allowedPeer {
 			logger.Info().Msg("Connection from disallowed peer")
 			s.Reset()
