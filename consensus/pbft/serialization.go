815a035841ccf94d81cfc548134cbd5212c0a790
Maelkum <aleksandar.cekrlic@gmail.com>
2023-08-15 19:55:08 +0200

    Add serialization/deserialization code for message types

--- consensus/pbft/serialization.go
+++ consensus/pbft/serialization.go
@@ -2,6 +2,9 @@ package pbft
 
 import (
 	"encoding/json"
+	"fmt"
+
+	"github.com/libp2p/go-libp2p/core/peer"
 )
 
 func (r Request) MarshalJSON() ([]byte, error) {
@@ -18,30 +21,50 @@ func (r Request) MarshalJSON() ([]byte, error) {
 		})
 }
 
+func (r *Request) UnmarshalJSON(data []byte) error {
+
+	request := struct {
+		Type MessageType     `json:"type"`
+		Data json.RawMessage `json:"data"`
+	}{}
+
+	err := json.Unmarshal(data, &request)
+	if err != nil {
+		return err
+	}
+
+	type alias *Request
+	return json.Unmarshal(request.Data, alias(r))
+}
+
 func (p PrePrepare) MarshalJSON() ([]byte, error) {
 
 	// Define aliases without the JSON marshaller.
-	type arequest Request
-	type alias struct {
-		View           uint     `json:"view"`
-		SequenceNumber uint     `json:"sequence_number"`
-		Digest         string   `json:"digest"`
-		Request        arequest `json:"request"`
-	}
-
+	type alias PrePrepare
 	return json.Marshal(
 		struct {
 			Type MessageType `json:"type"`
 			Data alias       `json:"data"`
 		}{
 			Type: MessagePrePrepare,
-			Data: alias{
-				View:           p.View,
-				SequenceNumber: p.SequenceNumber,
-				Digest:         p.Digest,
-				Request:        arequest(p.Request),
-			},
+			Data: alias(p),
 		})
+}
+
+func (p *PrePrepare) UnmarshalJSON(data []byte) error {
+
+	prePrepare := struct {
+		Type MessageType     `json:"type"`
+		Data json.RawMessage `json:"data"`
+	}{}
+
+	err := json.Unmarshal(data, &prePrepare)
+	if err != nil {
+		return err
+	}
+
+	type alias *PrePrepare
+	return json.Unmarshal(prePrepare.Data, alias(p))
 }
 
 func (p Prepare) MarshalJSON() ([]byte, error) {
@@ -56,6 +79,23 @@ func (p Prepare) MarshalJSON() ([]byte, error) {
 		})
 }
 
+func (p *Prepare) UnmarshalJSON(data []byte) error {
+
+	prepare := struct {
+		Type MessageType     `json:"type"`
+		Data json.RawMessage `json:"data"`
+	}{}
+
+	err := json.Unmarshal(data, &prepare)
+	if err != nil {
+		return err
+	}
+
+	// Aliased to prevent recursive call to UnmarshalJSON.
+	type aliased *Prepare
+	return json.Unmarshal(prepare.Data, aliased(p))
+}
+
 func (c Commit) MarshalJSON() ([]byte, error) {
 	type alias Commit
 	return json.Marshal(
@@ -68,6 +108,31 @@ func (c Commit) MarshalJSON() ([]byte, error) {
 		})
 }
 
+func (c *Commit) UnmarshalJSON(data []byte) error {
+
+	commit := struct {
+		Type MessageType     `json:"type"`
+		Data json.RawMessage `json:"data"`
+	}{}
+
+	err := json.Unmarshal(data, &commit)
+	if err != nil {
+		return err
+	}
+
+	// Aliased to prevent recursive call to UnmarshalJSON.
+	type aliased *Commit
+	return json.Unmarshal(commit.Data, aliased(c))
+}
+
+type prepareInfoEncoded struct {
+	View           uint               `json:"view"`
+	SequenceNumber uint               `json:"sequence_number"`
+	Digest         string             `json:"digest"`
+	PrePrepare     PrePrepare         `json:"preprepare"`
+	Prepares       map[string]Prepare `json:"prepares"`
+}
+
 func (p PrepareInfo) MarshalJSON() ([]byte, error) {
 
 	encodedPrepareMap := make(map[string]Prepare)
@@ -75,22 +140,43 @@ func (p PrepareInfo) MarshalJSON() ([]byte, error) {
 		encodedPrepareMap[id.String()] = prepare
 	}
 
-	type aliasPrePrepare PrePrepare
-	return json.Marshal(
-		struct {
-			View           uint               `json:"view"`
-			SequenceNumber uint               `json:"sequence_number"`
-			Digest         string             `json:"digest"`
-			PrePrepare     aliasPrePrepare    `json:"preprepare"`
-			Prepares       map[string]Prepare `json:"prepares"`
-		}{
-			View:           p.View,
-			SequenceNumber: p.SequenceNumber,
-			Digest:         p.Digest,
-			PrePrepare:     aliasPrePrepare(p.PrePrepare),
-			Prepares:       encodedPrepareMap,
-		},
-	)
+	rec := prepareInfoEncoded{
+		View:           p.View,
+		SequenceNumber: p.SequenceNumber,
+		Digest:         p.Digest,
+		PrePrepare:     p.PrePrepare,
+		Prepares:       encodedPrepareMap,
+	}
+
+	return json.Marshal(rec)
+}
+
+func (p *PrepareInfo) UnmarshalJSON(data []byte) error {
+
+	var info prepareInfoEncoded
+	err := json.Unmarshal(data, &info)
+	if err != nil {
+		return err
+	}
+
+	prepareMap := make(map[peer.ID]Prepare)
+	for idStr, prepare := range info.Prepares {
+		id, err := peer.Decode(idStr)
+		if err != nil {
+			return fmt.Errorf("could not decode peer.ID (str: %s): %w", idStr, err)
+		}
+		prepareMap[id] = prepare
+	}
+
+	*p = PrepareInfo{
+		View:           info.View,
+		SequenceNumber: info.SequenceNumber,
+		Digest:         info.Digest,
+		PrePrepare:     info.PrePrepare,
+		Prepares:       prepareMap,
+	}
+
+	return nil
 }
 
 func (v ViewChange) MarshalJSON() ([]byte, error) {
@@ -105,36 +191,84 @@ func (v ViewChange) MarshalJSON() ([]byte, error) {
 		})
 }
 
+func (v *ViewChange) UnmarshalJSON(data []byte) error {
+
+	vc := struct {
+		Type MessageType     `json:"type"`
+		Data json.RawMessage `json:"data"`
+	}{}
+
+	err := json.Unmarshal(data, &vc)
+	if err != nil {
+		return err
+	}
+
+	// Aliased to prevent recursive call to UnmarshalJSON.
+	type aliased *ViewChange
+	return json.Unmarshal(vc.Data, aliased(v))
+}
+
+type newViewEncode struct {
+	View        uint                  `json:"view"`
+	Messages    map[string]ViewChange `json:"messages"`
+	PrePrepares []PrePrepare          `json:"preprepares"`
+}
+
 func (v NewView) MarshalJSON() ([]byte, error) {
 	// To properly handle `peer.ID` serialization, this is a bit more involved.
 	// See documentation for `ResultMap.MarshalJSON` in `models/execute/response.go`.
-	type preprepareAlias PrePrepare
-	type newView struct {
-		View        uint                  `json:"view"`
-		Messages    map[string]ViewChange `json:"messages"`
-		PrePrepares []preprepareAlias     `json:"preprepares"`
-	}
 
 	nv := make(map[string]ViewChange)
 	for replica, vc := range v.Messages {
 		nv[replica.String()] = vc
 	}
 
-	preprepares := make([]preprepareAlias, 0, len(v.PrePrepares))
-	for _, pp := range v.PrePrepares {
-		preprepares = append(preprepares, preprepareAlias(pp))
-	}
-
 	return json.Marshal(
 		struct {
-			Type MessageType `json:"type"`
-			Data newView     `json:"data"`
+			Type MessageType   `json:"type"`
+			Data newViewEncode `json:"data"`
 		}{
 			Type: MessageNewView,
-			Data: newView{
+			Data: newViewEncode{
 				View:        v.View,
 				Messages:    nv,
-				PrePrepares: preprepares,
+				PrePrepares: v.PrePrepares,
 			},
 		})
+}
+
+func (n *NewView) UnmarshalJSON(data []byte) error {
+
+	newView := struct {
+		Type MessageType     `json:"type"`
+		Data json.RawMessage `json:"data"`
+	}{}
+
+	err := json.Unmarshal(data, &newView)
+	if err != nil {
+		return err
+	}
+
+	var nv newViewEncode
+	err = json.Unmarshal(newView.Data, &nv)
+	if err != nil {
+		return err
+	}
+
+	viewChangeMap := make(map[peer.ID]ViewChange)
+	for idStr, vc := range nv.Messages {
+		id, err := peer.Decode(idStr)
+		if err != nil {
+			return fmt.Errorf("could not decode peer.ID (str: %s): %w", idStr, err)
+		}
+		viewChangeMap[id] = vc
+	}
+
+	*n = NewView{
+		View:        nv.View,
+		Messages:    viewChangeMap,
+		PrePrepares: nv.PrePrepares,
+	}
+
+	return nil
 }
