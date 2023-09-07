a0ea6c1bf6b500777d9f2828da02c01bb4ab5159
Derek Anderson <dmikey@users.noreply.github.com>
2023-09-07 13:55:06 -0500

    update to keyforge and add sign and verify

---
+++ cmd/keyforge/README.md
@@ -0,0 +1,55 @@
+## KeyForge
+
+The `keyforge` utility is used for managing cryptographic keys and signing/verifying messages or data. It provides the following options:
+
+### Generate Keys
+
+Generate a new keypair and save it to a file:
+
+$ ./keyforge -o
+
+### Sign and Verify
+
+Sign and verify messages or data using your generated keys:
+
+#### Sign a Message
+
+Sign a message and save the signature:
+
+$ ./keyforge -s "Your message" -o
+
+#### Sign a File
+
+Sign a file and save the signature:
+
+$ ./keyforge -f -o
+
+#### Verify a Signature
+
+Verify a message or file's signature using the \`keyforge\` utility:
+
+$ ./keyforge -pubkey -message "Original message" -signature
+
+#### Verify a Signature with PeerID
+
+Verify a message or file's signature using a PeerID with the \`keyforge\` utility:
+
+$ ./keyforge -peerid -message "Original message" -signature
+
+#### Verify a Signature with OpenSSL
+
+Verify a message or file's signature using OpenSSL:
+
+##### Create a Signature
+
+Use OpenSSL to create a signature:
+
+$ openssl dgst -sha256 -sign -out message.sig
+
+##### Verify a Signature
+
+Use OpenSSL to verify a signature:
+
+$ openssl dgst -sha256 -verify -signature message.sig -in
+
+These commands enable you to manage cryptographic keys and perform signing and verification operations, including using OpenSSL for verification, conveniently within the Blockless b7s Node network.
---
+++ cmd/keyforge/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/pflag"
+)
+
+const (
+	privKeyName        = "priv.bin"
+	pubKeyName         = "pub.bin"
+	pubKeyTxtName      = "pubkey.txt"
+	identityName       = "identity"
+	peerIDFileName     = "peerid.txt"
+	privKeyPermissions = 0600
+	pubKeyPermissions  = 0644
+	
+)
+
+func main() {
+	var (
+		flagOutputDir string
+		flagString    string
+		flagFile      string
+		flagPublicKey string
+		flagMessage   string
+		flagSignature string
+		flagPeerID    string
+	)
+
+	pflag.StringVar(&flagPeerID, "peerid", "", "PeerID for verification")
+	pflag.StringVarP(&flagOutputDir, "output", "o", ".", "directory where keys should be stored")
+	pflag.StringVarP(&flagString, "string", "s", "", "string to sign and verify")
+	pflag.StringVarP(&flagFile, "file", "f", "", "file to sign and verify")
+	pflag.StringVar(&flagPublicKey, "pubkey", "", "Base64 encoded public key for verification")
+	pflag.StringVar(&flagMessage, "message", "", "The original message to verify")
+	pflag.StringVar(&flagSignature, "signature", "", "Base64 encoded signature to verify")
+
+	pflag.Parse()
+
+	// Initialize output directory
+	err := os.MkdirAll(flagOutputDir, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Could not create output directory: %s", err)
+	}
+
+	privKeyFile := filepath.Join(flagOutputDir, privKeyName)
+
+	priv, pub, err := LoadOrCreateKeys(privKeyFile, flagOutputDir)
+	if err != nil {
+		log.Fatalf("Error loading or creating keys: %s", err)
+	}
+
+	if flagString != "" || flagFile != "" {
+		HandleSignAndVerify(priv, pub, flagString, flagFile, flagOutputDir)
+	}
+
+	if flagPublicKey != "" && flagMessage != "" && flagSignature != "" {
+		VerifyGivenSignature(flagPublicKey, flagMessage, flagSignature)
+	}
+	
+	if flagPeerID != "" && flagMessage != "" && flagSignature != "" {
+		VerifyGivenSignatureWithPeerID(flagPeerID, flagMessage, flagSignature)
+	}
+}
