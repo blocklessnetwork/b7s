3e84732d9dd96aa20b21be5070d3e7fad78db261
zees-dev <dev.786zshan@gmail.com>
2024-06-28 19:56:29 +1200

    worker execute upd

--- node/worker_execute.go
+++ node/worker_execute.go
@@ -2,7 +2,6 @@ package node
 
 import (
 	"context"
-	"encoding/hex"
 	"fmt"
 	"time"
 
@@ -48,7 +47,6 @@ func (n *Node) workerProcessExecute(ctx context.Context, from peer.ID, req reque
 		Results: execute.ResultMap{
 			n.host.ID(): result,
 		},
-		Signature: hex.EncodeToString(result.Signature),
 	}
 
 	// Send the response, whatever it may be (success or failure).
@@ -61,16 +59,16 @@ func (n *Node) workerProcessExecute(ctx context.Context, from peer.ID, req reque
 }
 
 // workerExecute is called on the worker node to use its executor component to invoke the function.
-func (n *Node) workerExecute(ctx context.Context, requestID string, timestamp time.Time, req execute.Request, from peer.ID) (codes.Code, execute.Result, error) {
+func (n *Node) workerExecute(ctx context.Context, requestID string, timestamp time.Time, req execute.Request, from peer.ID) (codes.Code, execute.NodeExecutionResult, error) {
 
 	// Check if we have function in store.
 	functionInstalled, err := n.fstore.Installed(req.FunctionID)
 	if err != nil {
-		return codes.Error, execute.Result{}, fmt.Errorf("could not lookup function in store: %w", err)
+		return codes.Error, execute.NodeExecutionResult{}, fmt.Errorf("could not lookup function in store: %w", err)
 	}
 
 	if !functionInstalled {
-		return codes.NotFound, execute.Result{}, nil
+		return codes.NotFound, execute.NodeExecutionResult{}, nil
 	}
 
 	// Determine if we should just execute this function, or are we part of the cluster.
@@ -80,17 +78,17 @@ func (n *Node) workerExecute(ctx context.Context, requestID string, timestamp ti
 	// execution requests from the head node, so it shouldn't really tolerate errors/ambiguities.
 	consensus, err := parseConsensusAlgorithm(req.Config.ConsensusAlgorithm)
 	if err != nil {
-		return codes.Error, execute.Result{}, fmt.Errorf("could not parse consensus algorithm from the head node request, aborting (value: %s): %w", req.Config.ConsensusAlgorithm, err)
+		return codes.Error, execute.NodeExecutionResult{}, fmt.Errorf("could not parse consensus algorithm from the head node request, aborting (value: %s): %w", req.Config.ConsensusAlgorithm, err)
 	}
 
 	// We are not part of a cluster - just execute the request.
 	if !consensusRequired(consensus) {
-		res, err := n.executor.ExecuteFunction(requestID, req)
+		res, meta, err := n.executor.ExecuteFunction(requestID, req)
 		if err != nil {
-			return res.Code, res, fmt.Errorf("execution failed: %w", err)
+			return res.Code, execute.NodeExecutionResult{Result: res, Metadata: meta}, fmt.Errorf("execution failed: %w", err)
 		}
 
-		return res.Code, res, nil
+		return res.Code, execute.NodeExecutionResult{Result: res, Metadata: meta}, nil
 	}
 
 	// Now we KNOW we need a consensus. A cluster must already exist.
@@ -100,7 +98,7 @@ func (n *Node) workerExecute(ctx context.Context, requestID string, timestamp ti
 	n.clusterLock.RUnlock()
 
 	if !ok {
-		return codes.Error, execute.Result{}, fmt.Errorf("consensus required but no cluster found; omitted cluster formation message or error forming cluster (request: %s)", requestID)
+		return codes.Error, execute.NodeExecutionResult{}, fmt.Errorf("consensus required but no cluster found; omitted cluster formation message or error forming cluster (request: %s)", requestID)
 	}
 
 	log := n.log.With().Str("request", requestID).Str("function", req.FunctionID).Str("consensus", consensus.String()).Logger()
@@ -109,10 +107,10 @@ func (n *Node) workerExecute(ctx context.Context, requestID string, timestamp ti
 
 	code, value, err := cluster.Execute(from, requestID, timestamp, req)
 	if err != nil {
-		return codes.Error, execute.Result{}, fmt.Errorf("execution failed: %w", err)
+		return codes.Error, execute.NodeExecutionResult{}, fmt.Errorf("execution failed: %w", err)
 	}
 
 	log.Info().Str("code", string(code)).Msg("node processed the execution request")
 
-	return code, value, nil
+	return code, execute.NodeExecutionResult{Result: value}, nil
 }
